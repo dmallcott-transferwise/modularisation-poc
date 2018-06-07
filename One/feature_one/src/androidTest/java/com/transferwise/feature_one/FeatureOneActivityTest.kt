9c22a426b5f1325c299cbcd85d16b33200d34b00
Daniel Mallcott <daniel.mallcott@transferwise.com>
2018-06-07 13:17:57 +0200

    More Ui test stuff

--- One/feature_one/src/androidTest/java/com/transferwise/feature_one/FeatureOneActivityTest.kt
+++ One/feature_one/src/androidTest/java/com/transferwise/feature_one/FeatureOneActivityTest.kt
@@ -1,16 +1,38 @@
 package com.transferwise.feature_one
 
+import android.support.test.InstrumentationRegistry.getInstrumentation
 import android.support.test.rule.ActivityTestRule
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mockito
+import android.support.test.InstrumentationRegistry.getTargetContext
+import com.transferwise.network.response.FeatureOneResponse
+
 
 class FeatureOneActivityTest {
+
     @Rule
     @JvmField
     val rule = ActivityTestRule(FeatureOneActivity::class.java, false, false)
 
+    lateinit var app: UiTestApp
+
+    @Before
+    fun setUp() {
+        app = getInstrumentation().targetContext.applicationContext as UiTestApp
+    }
+
     @Test
     fun name() {
+        val response = "iOS sucks!"
+        Mockito.`when`(app.provide().featureOne()).thenReturn(FeatureOneResponse(response))
+
         rule.launchActivity(null)
+
+        InviteViewActions.clickApiCall()
+        InviteViewActions.checkToastIsDisplayed(rule.activity.window.decorView, response)
+
+        Thread.sleep(3000)
     }
 }
---
+++ One/feature_one/src/androidTest/java/com/transferwise/feature_one/InviteViewActions.kt
@@ -0,0 +1,25 @@
+package com.transferwise.feature_one
+
+import android.support.test.espresso.matcher.RootMatchers.withDecorView
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.not
+import android.view.View
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.matcher.ViewMatchers.*
+
+
+object InviteViewActions {
+    fun checkToastIsDisplayed(decorView: View, errorMessage: String) {
+        // decorView is needed since it's a Toast
+        onView(withText(errorMessage))
+                .inRoot(withDecorView(not(`is`(decorView))))
+                .check(matches(isDisplayed()))
+    }
+
+    fun clickApiCall() {
+        onView(withId(R.id.apiCall)).perform(click())
+    }
+}
